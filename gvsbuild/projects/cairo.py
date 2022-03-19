c0927f9b311c67991033b361405d6b60fea6428b
Dan Yeaw <dan@yeaw.me>
2022-03-19 11:04:28 -0400

    Update cairo to version 1.17.4

--- gvsbuild/projects/cairo.py
+++ gvsbuild/projects/cairo.py
@@ -15,55 +15,23 @@
 #  You should have received a copy of the GNU General Public License
 #  along with this program; if not, see <http://www.gnu.org/licenses/>.
 
-import os
-
+from gvsbuild.utils.base_builders import Meson
 from gvsbuild.utils.base_expanders import Tarball
 from gvsbuild.utils.base_project import Project, project_add
 
 
 @project_add
-class Cairo(Tarball, Project):
+class Cairo(Tarball, Meson):
     def __init__(self):
         Project.__init__(
             self,
             "cairo",
-            archive_url="https://cairographics.org/releases/cairo-1.16.0.tar.xz",
-            hash="5e7b29b3f113ef870d1e3ecf8adf21f923396401604bda16d44be45e66052331",
+            archive_url="https://gitlab.freedesktop.org/cairo/cairo/-/archive/1.17.4/cairo-1.17.4.tar.gz",
+            hash="0a673c88f7c049b4555c1f152b11d9fe16d8523b62632d2d5c14e1868499ce9f",
             dependencies=["fontconfig", "glib", "pixman", "libpng"],
-            patches=["0001-Fix-mask-usage-in-image-compositor.patch"],
+            patches=[],
         )
 
     def build(self):
-        self.exec_vs(
-            r"make -f Makefile.win32 CFG=%(configuration)s ARCH=%(platform)s",
-            add_path=os.path.join(self.builder.opts.msys_dir, "usr", "bin"),
-        )
-        self.push_location(r".\util\cairo-gobject")
-        self.exec_vs(
-            r"make -f Makefile.win32 CFG=%(configuration)s ARCH=%(platform)s",
-            add_path=os.path.join(self.builder.opts.msys_dir, "usr", "bin"),
-        )
-        self.pop_location()
-
-        self.install(r".\src\%(configuration)s\cairo.dll bin")
-        self.install(r".\util\cairo-gobject\%(configuration)s\cairo-gobject.dll bin")
-
-        self.install(r".\src\%(configuration)s\cairo.lib lib")
-        self.install(r".\util\cairo-gobject\%(configuration)s\cairo-gobject.lib lib")
-
-        self.install(r".\src\cairo.h include\cairo")
-        self.install(r".\src\cairo-deprecated.h include\cairo")
-        self.install(r".\src\cairo-pdf.h include\cairo")
-        self.install(r".\src\cairo-ps.h include\cairo")
-        self.install(r".\src\cairo-script.h include\cairo")
-        self.install(r".\src\cairo-svg.h include\cairo")
-        self.install(r".\src\cairo-tee.h include\cairo")
-        self.install(r".\src\cairo-win32.h include\cairo")
-        self.install(r".\src\cairo-xml.h include\cairo")
-        self.install(r".\src\cairo-ft.h include\cairo")
-        self.install(r".\src\cairo-features.h include\cairo")
-        self.install(r".\util\cairo-gobject\cairo-gobject.h include\cairo")
-        self.install(r".\cairo-version.h include\cairo")
-
-        self.install_pc_files()
+        Meson.build(self, meson_params="-Dgtk_docs=false")
         self.install(r".\COPYING share\doc\cairo")
